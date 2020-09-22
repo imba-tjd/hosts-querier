898aa852f25409dbc5da25918f870f4b3b2d0dc6
imba-tjd <109224573@qq.com>
2020-09-22 08:35:55 +0800

    text: host -> domain

--- query.py
+++ query.py
@@ -7,23 +7,23 @@ api = 'https://dns.google.com/resolve?name={}&type=A&ecs=111.0.0.0'
 helpmsg = '''Automatically query hosts from Google DNS.
     Usage: hosts_query.py domains.txt > hosts
        Or: echo example.com | hosts_query.py
-       Or: hosts_query.py then input one host for a line and then ctrl+Z
+       Or: hosts_query.py then input one domain for a line and then ctrl+Z
     Empty lines and comments will be ignored.
     Chain compact.py if you want.'''
 
 
-def resolve(host: str):
-    '''query one host'''
-    response = urlopen(api.format(host), timeout=5).read().decode('utf-8')
+def resolve(domain: str):
+    '''query one domain'''
+    response = urlopen(api.format(domain), timeout=5).read().decode('utf-8')
     data = json.loads(response)
     if data['Status'] == 0:
-        return (ans['data']+' '+host for ans in data['Answer'])
+        return (ans['data']+' '+domain for ans in data['Answer'])
     else:
-        logging.warning(f'resolve {host} failed.')
+        logging.warning(f'resolve {domain} failed.')
 
 
 if __name__ == "__main__":
     to_resolve = get_to_resolve(handle_args(helpmsg))
-    for host in to_resolve:
-        for entry in resolve(host):
+    for domain in to_resolve:
+        for entry in resolve(domain):
             print(entry)
